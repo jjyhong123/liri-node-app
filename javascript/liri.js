654eb72547dbe478c2bd20b1bb420f87b2186b2e
Jae <jjyhong123@gmail.com>
2018-07-23 17:53:19 -0700

    added twitter

--- javascript/liri.js
+++ javascript/liri.js
@@ -3,5 +3,18 @@ var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-
 console.log(spotify);
+
+switch (process.argv[2]) {
+    case "my-tweets":
+        twitter();
+        break;
+    case "spotify-this-song": 
+        spotify();
+        break;
+    case "movie-this":
+        OMDB();
+        break;
+    case "do-what-it-says":
+        random();
+}
